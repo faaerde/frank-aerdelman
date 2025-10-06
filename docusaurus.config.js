8a920f9781c9c0b6d4ed95330ce36e19a64aef68
Frank Aerdelman <Frank.Aerdelman@gmail.com>
2025-10-06 14:19:06 -0700

    LinkedIn link in footer.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -111,16 +111,8 @@ const config = {
             title: 'Community',
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
+                label: 'LinkedIn',
+                href: 'https://www.linkedin.com/in/frank-aerdelman/',
               },
             ],
           },
